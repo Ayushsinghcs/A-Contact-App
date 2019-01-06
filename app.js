e2e3c34d85e7066b9164cf833e09241a073f049e
Ayush Singh <41228583+Ayushsinghcs@users.noreply.github.com>
2019-01-06 20:46:25 +0530

    Update app.js

--- app.js
+++ app.js
@@ -59,7 +59,7 @@ app.post('/details.html', (req, res) => {
 });
 
 // Define port
-const port = 3000;
+const port=Number(process.env.PORT || 3000);
 
 // Start server
 const server = app.listen(port, () => console.log(`Server started on port ${port}`));
