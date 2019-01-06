5c8040d64395e57ea40878d7bcae8e9652a897cf
Ayush Singh <41228583+Ayushsinghcs@users.noreply.github.com>
2019-01-06 20:10:20 +0530

    Update app.js

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + '/public/index.html'));
 });
 
